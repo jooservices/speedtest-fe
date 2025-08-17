cc0fee90c5653099bfd879d1e80cf2742d921a0c
justin.huynh <justin.huynh3009@gmail.com>
2025-08-17 22:43:28 +0700

    Fix format function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 
 import HomePage from './HomePage'
+import Dashboard from './Dashboard'
 
 function PageRoutes() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/dashboard' element={<HomePage />} />
+      <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/chart' element={<>Chart Page</>} />
       <Route path='*' element={<>NOT FOUND</>} />
     </Routes>
--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,9 +1,28 @@
-export function formatSpeed(bps: number, includeUnit = true): string {
-  if (bps >= 1_000_000) {
-    return `${(bps / 1_000_000).toFixed(2)}` + (includeUnit ? ' Mbps' : '')
-  } else if (bps >= 1_000) {
-    return `${(bps / 1_000).toFixed(2)}` + (includeUnit ? ' Kbps' : '')
-  } else {
-    return `${bps}` + (includeUnit ? ' bps' : '')
+export function formatSpeed(bps: number, includeUnit = true, convertTo: string = 'mbps'): string {
+  let speedInMbps = bps / 125_000;
+
+  switch (convertTo) {
+    case 'bps':
+      return `${bps}` + (includeUnit ? ' bps' : '');
+    case 'kbps':
+      return `${(speedInMbps * 1_000).toFixed(2)}` + (includeUnit ? ' Kbps' : '');
+    case 'gbps':
+      return `${(speedInMbps / 1_000).toFixed(2)}` + (includeUnit ? ' Gbps' : '');
+    default:
+      return `${(speedInMbps).toFixed(2)}` + (includeUnit ? ' Mbps' : '');
+  }
+}
+
+export function formatPing(pingInMb: number, includeUnit = true, convertTo: string = 'mb'): string {
+
+  switch (convertTo) {
+    case 'bps':
+      return `${pingInMb * 1_000_000}` + (includeUnit ? ' bps' : '');
+    case 'kbps':
+      return `${(pingInMb * 1_000).toFixed(2)}` + (includeUnit ? ' Kbps' : '');
+    case 'gbps':
+      return `${(pingInMb / 1_000).toFixed(2)}` + (includeUnit ? ' Gbps' : '');
+    default:
+      return `${(pingInMb).toFixed(2)}` + (includeUnit ? ' Mbps' : '');
   }
 }
